8b07ece713fbf69c1610ce10b6410ce36b800677
nmckimm <35746958+nmckimm@users.noreply.github.com>
2018-02-22 23:18:58 +0000

    Testing again

---
+++ systeminfo/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'systeminfo'
+project = "sysinfo_with_flask"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Displays system information through flask on web browser'
+authors = ['Neil McKimm','Aonghus Lawlor ']
+authors_string = ', '.join(authors)
+emails = ['neil.mckimm@ucdconnect.ie', 'aonghus.lawlor@ucd.ie']
+license = 'MIT'
+copyright = '2018 ' + authors_string
+url = 'http://github.com/nmckimm'
---
+++ top/display.py
@@ -0,0 +1,13 @@
+'''
+Created on 22 Feb 2018
+
+@author: neil
+'''
+__import__ data
+
+
+
+
+
+if __name__ == '__main__':
+    data.sysinfo()
